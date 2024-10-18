0699bb510ebcde0bed04051cbcee997546863da7
Pozzame <pozzame@gmail.com>
2024-10-18 17:54:00 +0200

    first commit

---
+++ ChatClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+public class ChatClient
+    {
+        void StartClient(string serverIP, int port)
+        {
+            using (var client = new TcpClient(serverIP, port));
+            using (var stream = client.GetStream())
+            {
+                Console.WriteLine("Connesso al server");
+                string messageToSend = Console.ReadLine();
+                while (!string.IsNullOrEmpty(messageToSend))
+                {
+                    ping google.it
+                }
+            }
+        }
+    }
